a378ed1cea14e31d59a41a423791d38c735a31b1
Topping <c_topping@tss-hr420-dm32.fc.ca>
2018-02-15 10:36:07 -0500

    these are the updated files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 (() => {
-  console.log('fired');
+  console.log('fired! JS is FUN!');
+
+//variables always go at the top
+  var iconSet = document.querySelectorAll('svg');
+
+//function goes here (inbetween)
+function logThisIcon() {
+  console.log('clicked on this icon: ') this.id);
+
+  //change opacity
+  thic.style.opacity = 0.5;
+}
+
+//event handling goes at the bottom
+  iconSet.forEach(icon => icon.addEventListener('click',logThisIcon));
 })();
